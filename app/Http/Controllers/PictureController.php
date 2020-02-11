4b590daa4fa12ef2c09270a594381dfd72862096
Pascal Hurni <phi@pragmaticpurist.org>
2020-02-11 10:33:11 +0100

    Add index view

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -15,7 +15,8 @@ class PictureController extends Controller
      */
     public function index()
     {
-        //
+      $pictures = Picture::all();
+      return view('pictures.index', compact('pictures'));
     }
 
     /**

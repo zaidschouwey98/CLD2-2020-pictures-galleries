8ad66a94562ed76267e490ba00ab2026dac03842
Pascal Hurni <phi@pragmaticpurist.org>
2020-03-03 10:30:16 +0100

    Add layout and prepare js stack

--- resources/views/pictures/create.blade.php
+++ resources/views/pictures/create.blade.php
@@ -1,18 +1,18 @@
-<form action="{{ $s3attributes['action'] }}" method="POST" enctype="multipart/form-data">
-  <!--
+@extends('layouts/app')
+
+@push('scripts')
+  <script src="/js/s3upload.js"></script>
+@endpush
+
+@section('content')
+<form action="{{ route('pictures.store') }}" method="POST" enctype="multipart/form-data"
+data-s3-attributes="{{ json_encode($s3attributes) }}" data-s3-inputs="{{ json_encode($s3inputs) }}">
   @csrf
   <input type="text" name="title" />
   @error('title')
       <div class="alert alert-danger">{{ $message }}</div>
   @enderror
-    -->
-  @foreach($s3inputs as $name => $value)
-    <input type="hidden" name="{{ $name }}" value="{{ $value }}">
-  @endforeach
-    
   <input type="file" name="file"/>
-  @error('picture')
-      <div class="alert alert-danger">{{ $message }}</div>
-  @enderror
   <input type="submit" />
 </form>
+@endsection

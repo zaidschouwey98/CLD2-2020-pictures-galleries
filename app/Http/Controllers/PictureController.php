191d4e24cb66908a8f941cec9afb93f8c402bcf3
Pascal Hurni <phi@pragmaticpurist.org>
2020-03-03 09:59:06 +0100

    Prove that the file upload to s3 works with barebone HTML

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -26,7 +26,26 @@ class PictureController extends Controller
      */
     public function create()
     {
-      return view('pictures.create');
+      $awsClient = new \Aws\S3\S3Client([
+        'version' => 'latest',
+        'region' => env('AWS_DEFAULT_REGION'),
+      ]);
+      $bucket = env('AWS_BUCKET');
+      $key = "pictures/" . \Str::random(40);
+      $formInputs = ['acl' => 'private', 'key' => $key];
+      $options = [
+          ['acl' => 'private'],
+          ['bucket' => $bucket],
+          ['eq', '$key', $key],
+      ];      
+      $postObject = new \Aws\S3\PostObjectV4(
+        $awsClient, $bucket, $formInputs, $options, "+1 hours"
+      );
+      
+      return view('pictures.create', [
+        's3attributes' => $postObject->getFormAttributes(),
+        's3inputs' => $postObject->getFormInputs(),
+      ]);
     }
 
     /**
--- resources/views/pictures/create.blade.php
+++ resources/views/pictures/create.blade.php
@@ -1,10 +1,16 @@
-<form action="{{ route('pictures.store') }}" method="POST" enctype="multipart/form-data">
+<form action="{{ $s3attributes['action'] }}" method="POST" enctype="multipart/form-data">
+  <!--
   @csrf
   <input type="text" name="title" />
   @error('title')
       <div class="alert alert-danger">{{ $message }}</div>
   @enderror
-  <input type="file" name="picture"/>
+    -->
+  @foreach($s3inputs as $name => $value)
+    <input type="hidden" name="{{ $name }}" value="{{ $value }}">
+  @endforeach
+    
+  <input type="file" name="file"/>
   @error('picture')
       <div class="alert alert-danger">{{ $message }}</div>
   @enderror
